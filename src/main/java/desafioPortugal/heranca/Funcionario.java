21feff106967248d275b4eb4a73d84022550f577
Edilson do Nascimento <edilsonjoper@gmail.com>
2023-06-17 11:50:55 -0300

    07 - Teste Herança

---
+++ src/main/java/desafioPortugal/heranca/Funcionario.java
@@ -0,0 +1,8 @@
+package desafioPortugal.heranca;
+
+public class Funcionario extends Pessoa{
+
+    public Funcionario(String nome, int idade) {
+        super(nome, idade);
+    }
+}
---
+++ src/main/java/desafioPortugal/heranca/MainHeranca.java
@@ -0,0 +1,10 @@
+package desafioPortugal.heranca;
+
+public class MainHeranca {
+    public static void main(String[] args) {
+        Pessoa func = new Funcionario("Pedro", 32);
+
+        System.out.println(func.getNome());
+
+    }
+}
